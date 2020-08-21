223046579b42586f362b6a41f590a4bc5cf51bdf
TOPORIDERS <67081469+TOPORIDERS@users.noreply.github.com>
2020-08-20 23:11:17 -0400

    Initial commit

---
+++ app/src/main/java/com/example/nico2008/model/IVerifie.java
@@ -0,0 +1,7 @@
+package com.example.nico2008.model;
+
+public interface IVerifie {
+    boolean evalueteLength (String password);
+
+    boolean evalueteUpper  (String password);
+}
---
+++ app/src/main/java/com/example/nico2008/model/Verifie.java
@@ -0,0 +1,27 @@
+package com.example.nico2008.model;
+
+public class Verifie implements  IVerifie {
+
+        public static final int MAIN_LENGTH = 5
+        public static final int WEAK
+        public static final int
+        public static final int
+
+
+
+
+
+
+
+
+
+    @Override
+    public boolean evalueteLength(String password) {
+        return false;
+    }
+
+    @Override
+    public boolean evalueteUpper(String password) {
+        return false;
+    }
+}
